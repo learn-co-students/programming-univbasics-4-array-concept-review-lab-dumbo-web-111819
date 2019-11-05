b9b3e1c4f5a64109e7bbfc0c952009c0674f9afa
Annie Zheng <anniezheng.anniee@gmail.com>
2019-11-05 02:38:00 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,9 +7,7 @@ def find_element_index(array, value_to_find)
       counter += 1
     end
   end
-  if array[counter] != value_to_find
-    return nil
-  end
+   nil
 end
 
 def find_max_value(array)
